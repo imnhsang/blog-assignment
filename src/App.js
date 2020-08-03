7be011a86650923d587be5582594245902c14549
Leo Nguyen <hoangsang160898@gmail.com>
2020-08-03 11:05:16 +0700

    update redux, react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<Router>
+			<Switch>
+				<Route />
+			</Switch>
+		</Router>
+	)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from './reducers'
+
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(thunkMiddleware))
+)
 
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById('root')
 )
