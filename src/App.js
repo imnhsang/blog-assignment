1c6c856b08afbb921563cce0a022e56fe0325ad3
Leo Nguyen <hoangsang160898@gmail.com>
2020-08-05 17:26:28 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route />
-			</Switch>
-		</Router>
-	)
+	return <h1>Hello</h1>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { Provider } from 'react-redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import rootReducer from './reducers'
 
-const store = createStore(
-	rootReducer,
-	composeWithDevTools(applyMiddleware(thunkMiddleware))
-)
+import App from './App'
+
+import './index.scss'
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
-	<Provider store={store}>
+	<React.StrictMode>
 		<App />
-	</Provider>,
+	</React.StrictMode>,
 	document.getElementById('root')
 )
