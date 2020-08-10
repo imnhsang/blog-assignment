df407ab64973b6a6707789cfe7d86c9e2e0b6520
Leo Nguyen <hoangsang160898@gmail.com>
2020-08-10 16:41:44 +0700

    test push

---
+++ src/components/Input/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Input } from 'semantic-ui-react'
+
+const InputDefault = ({ placeholder }) => {
+	return <Input placeholder={placeholder} />
+}
+
+export default InputDefault
--- src/pages/SigninPage/index.js
+++ src/pages/SigninPage/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import Header from 'components/HeaderPage'
+import Footer from 'components/FooterPage'
+
 const SigninPage = () => {
-	return <div className='sign-page'></div>
+	return (
+		<div className='sign-page'>
+			<Header />
+			<Footer />
+		</div>
+	)
 }
 
 export default SigninPage
