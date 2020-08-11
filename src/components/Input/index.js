8dab7be518f51bb702fa1ae63c291affef3413b3
Leo Nguyen <hoangsang160898@gmail.com>
2020-08-11 10:48:46 +0700

    update ui signin

--- src/components/Input/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Input } from 'semantic-ui-react'
-
-const InputDefault = ({ placeholder }) => {
-	return <Input placeholder={placeholder} />
-}
-
-export default InputDefault
--- src/pages/SigninPage/index.js
+++ src/pages/SigninPage/index.js
@@ -2,11 +2,21 @@ import React from 'react'
 
 import Header from 'components/HeaderPage'
 import Footer from 'components/FooterPage'
+import FormSignin from 'pages/SigninPage/containers/FormSignin'
+
+import './style.scss'
+import 'assets/stylesheets/global.scss'
 
 const SigninPage = () => {
 	return (
-		<div className='sign-page'>
+		<div className='signin-page'>
 			<Header />
+			<div
+				className='d-flex justify-content-center align-items-center h-100'
+				style={{ marginTop: '4.75rem' }}
+			>
+				<FormSignin />
+			</div>
 			<Footer />
 		</div>
 	)
