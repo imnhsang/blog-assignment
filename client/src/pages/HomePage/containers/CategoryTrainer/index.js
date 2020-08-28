27c1a81cde87a8e100556a7b5e81e37d9a9de612
Leo Nguyen <hoangsang160898@gmail.com>
2020-08-28 09:21:55 +0700

    add loading category

--- client/src/pages/HomePage/containers/CategoryTrainer/index.js
+++ client/src/pages/HomePage/containers/CategoryTrainer/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
+import { Loader } from 'semantic-ui-react'
 import ItemCategoryTrainer from 'pages/HomePage/components/Item/CategoryTrainer'
 // import Button from 'components/Button/TransparentArrowDown'
 
 import './style.scss'
 
 const AreaCategoryTrainer = ({ data }) => {
+	const loadingGetCategory = useSelector((state) => state.category.loading)
 	return (
 		<div className='area-category-trainer row flex-column items-center'>
 			<p className='area-category-trainer__title text-center'>
 				GAIN ACESS TO A NETWORK OF TOP TRAINERS
 			</p>
+			<Loader active={loadingGetCategory} inline='centered' />
 			<div className='row area-category-trainer__list-category'>
 				{data &&
 					data.map((e, inx) => (
